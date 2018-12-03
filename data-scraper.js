b5861bf3f35cdc94f0990fe7592dfbcd67f75272
christopherchateau <christopherchateau@gmail.com>
2018-12-03 16:33:36 -0700

    Run nightmare to scrape beer descriptions, create beer-descriptions.js in db/scraped-data.

--- data-scraper.js
+++ data-scraper.js
@@ -5,14 +5,14 @@ var fs = require("fs");
 nightmare
   .goto("http://cerebralbrewing.com/beer/")
   .evaluate(function() {
-    let titles = document.querySelectorAll("p");
+    let titles = document.querySelectorAll("p[style=\'text-align: left;'\]");
     let list = [].slice.call(titles);
     let results = list.map(node => node.innerText);
-    return results.filter(str => str.includes("% ABV"));
+    return results.filter(str => !str.includes("% ABV"));
   })
   .end()
   .then(function(result) {
-    fs.writeFileSync("./db/scraped-data/beer-abv.js", JSON.stringify(result));
+    fs.writeFileSync("./db/scraped-data/beer-descriptions.js", JSON.stringify(result));
     console.log(result);
   })
   .catch(error => console.error("Scrape Failed", error));
