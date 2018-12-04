3c8a463562d245e432b24378b70c5cdfef01c9d2
christopherchateau <christopherchateau@gmail.com>
2018-12-03 18:13:39 -0700

    Do first migration, create beers and beer_styles tables

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports = {
   development: {
     client: "pg",
-    connection: "postgres://localhost/BYOBE",
+    connection: "postgres://localhost/byobe",
     migrations: {
       directory: "./db/migrations"
     },
