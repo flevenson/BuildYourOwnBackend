bf11c090ed7e09b06e8e62aecacd8d2fe89d50cf
“Freddie <freddielev@gmail.com>
2018-12-04 14:36:47 -0700

    Add column for beer name

--- db/seeds/dev/cerebral_beers.js
+++ db/seeds/dev/cerebral_beers.js
@@ -1,13 +1,46 @@
+const beerData = require('../../../public/cleaner.js');
+const beers = beerData[0];
+const styles = beerData[1];
+// const environment = process.env.NODE_ENV || 'development';
+// const config = require('../../../knexfile')[environment]
+// const database = require('knex')(config);
 
+const createStyles = (knex, style) => {
+
+  return knex('beer_styles')
+    .insert({
+      style_name: style.beerStyle,
+      description: style.description
+    }, 'id')
+    .then(styleID => {
+      let beerPromises = beers.map(beer => {
+        return knex('beers').insert({
+          abv: beer.abv,
+          description: beer.description,
+          is_available: availability,
+          style_id: styleID[0]
+        })
+      })
+    })
+}
+
+// const createBeer = (knex, beer) => {
+//   console.log(database('beer_styles').select('id'))
+//   // return knex('beers')
+//   //   .insert()
+// }
 
 exports.seed = function(knex, Promise) {
-  return knex("table_name")
+  return knex("beers")
     .del()
-    .then(function() {
-      return knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
-      ]);
-    });
+    .then(() => knex("beer_styles").del())
+    .then(() => {
+      const stylePromises = styles.map(style => {
+        return createStyles(knex, style)
+      })
+    return Promise.all(stylePromises)
+    })
+    // .then(createBeer)
+    .then(() => console.log('Great Success!!'))
+    .catch(error => console.log(`Error seeding data: ${error}`))
 };
